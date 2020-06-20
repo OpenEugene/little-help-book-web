e71b4583208c6e7a0e109fdee4d9e5ab3b8f7797
Mark Davis <311063+nohorse@users.noreply.github.com>
2020-06-20 15:06:36 -0700

    added the model for Help Services

---
+++ LittleHelpBook/Shared/Data/HelpModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using AirtableApiClient;
+using Newtonsoft.Json;
+
+namespace EugeneFoodScene.Data
+{
+
+    // Places, Categories, Cuisines, Delivery Services, Order Delivery Links
+
+    /// <summary>
+    /// interface for hoisting Airtable IDs into data objests
+    /// </summary>
+    public interface IAirtable
+    {
+        public string Id { get; set; }
+    }
+
+
+    /// <summary>
+    /// Name, Category, Phone, URL, Address, Notes, Order Delivery Links, Menu,
+    /// </summary>
+    public class Help : IAirtable
+    {
+        public string Id { get; set; }
+        [JsonProperty("Service Name")]
+        public string Name { get; set; }
+        [JsonProperty("Category")]
+        public string Category { get; set; }
+       
+    }
+
+  
+    
+}
