7f8ef40bf52292ab172f99aaa04eddafb40d659c
Mark Davis <github@nohorse.com>
2020-06-20 15:15:25 -0700

    added airtable service

---
+++ LittleHelpBook/Server/Services/AirTableBase.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using AirtableApiClient;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using EugeneFoodScene.Data;
+//using Markdig.Extensions.ListExtras;
+
+
+namespace EugeneFoodScene.Services
+{
+
+
+    /// <summary>
+    /// a bruit force airtable data caching service
+    /// </summary>
+    public abstract class AirTableBase
+    {
+        protected readonly string BaseId;
+        protected readonly string AppKey;
+
+        private static SemaphoreSlim semaphore = new SemaphoreSlim(1);
+
+        public AirTableBase(IConfiguration configuration)
+        {
+            BaseId = configuration["AirTable:BaseId"];
+            AppKey = configuration["AirTable:AppKey"];
+        }
+
+        private void PopulateIds<T>(IEnumerable<AirtableRecord<T>> responseRecords) where T : IAirtable
+        {
+            foreach (var item in responseRecords)
+            {
+                item.Fields.Id = item.Id;
+            }
+        }
+
+        protected async Task<IEnumerable<T>> GetTableAsync<T>(string tableName) where T : IAirtable
+        {
+            var table = new List<AirtableRecord<T>>();
+            string offset = null;
+            var retryCount = 0;
+
+            semaphore.Wait(); // wait for your place in line
+
+            using AirtableBase airtableBase = new AirtableBase(AppKey, BaseId);
+            try
+            {
+                do
+                {
+                    Task<AirtableListRecordsResponse<T>> task =
+                        airtableBase.ListRecords<T>(tableName: tableName, offset: offset);
+
+                    var response = await task;
+
+                    if (response.Success)
+                    {
+                        PopulateIds(response.Records);
+                        table.AddRange(response.Records);
+                        offset = response.Offset;
+                    }
+                    // look for timeouts and add retry logic. 
+                    else if (response.AirtableApiError is AirtableApiException)
+                    {
+                        if (retryCount < 3)
+                        {
+                            retryCount++;
+                            await Task.Delay(500); //pause half-a-sec
+                          
+                        }
+                        else
+                        {
+                            // too many retrys
+                            table = null;
+                            break;
+                        }
+                    }
+
+                } while (offset != null);
+            }
+            catch (Exception e)
+            {
+
+                table = null;
+            }
+            finally
+            {
+                semaphore.Release(); // let the next one in
+            }
+
+            List<T> list = null;
+            if (table != null)
+            {
+                list = new List<T>();
+                try
+                {
+                    list = (from c in table select c.Fields).ToList();
+                }
+                catch (Exception e)
+                {
+                    var err = e.InnerException;
+                }
+            }
+
+            return list.AsEnumerable();
+        }
+    }
+}
---
+++ LittleHelpBook/Server/Services/AirTableService.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EugeneFoodScene.Data;
+using EugeneFoodScene.Services;
+using GoogleMapsComponents.Maps;
+using Microsoft.Extensions.Configuration;
+
+namespace LittleHelpBook.Server.Services
+{
+    /// <summary>
+    /// a bruit force airtable data caching service
+    /// </summary>
+    public class AirTableService : AirTableBase
+    {
+        private IEnumerable<Help> _helpServices;  // the higest level populated places list.
+    
+
+        public AirTableService(IConfiguration configuration) : base(configuration) {}
+
+        public async Task<IEnumerable<Help>> GetHelpServicesAsync()
+        {
+            return _helpServices ??= await GetTableAsync<Help>("Help");
+        }
+
+
+    }
+}
