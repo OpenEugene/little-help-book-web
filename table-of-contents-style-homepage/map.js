974377166da3a3f8b83bdb97fe770b94a19d5a25
daniel coats <daniel.e.coats@gmail.com>
2020-07-31 16:38:33 -0700

    Got the tile-set working from openstreetmaps.org

--- table-of-contents-style-homepage/map.js
+++ table-of-contents-style-homepage/map.js
@@ -1,5 +1,8 @@
 function mapInit() {
 	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	var attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+	var tiles = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { attribution });
+	tiles.addTo(mymap);
 }
 
 mapInit();
