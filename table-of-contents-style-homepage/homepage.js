8e684d3a0c815cbce1a24788405c9086ddfe1bd9
Arthur Smid <smidarthur@gmail.com>
2020-08-02 13:55:45 -0700

    begin emergency crisis lines button menu

--- table-of-contents-style-homepage/homepage.js
+++ table-of-contents-style-homepage/homepage.js
@@ -6,16 +6,15 @@ function crisisPopup() {
     popup.classList.toggle("show");
   }
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
 function cityMenu() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
+// Close the dropdown menu if the user clicks outside of it, I haven't got this to work yet:
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+  if (!event.target.matches('.dropdown-button')) {
+    var dropdowns = document.getElementsByClass("list-of-cities");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
@@ -25,3 +24,9 @@ window.onclick = function(event) {
     }
   }
 } 
+
+function emergencyCrisis() {
+  document.getElementById("emerNumbers").classList.toggle("show");
+}
+
+
