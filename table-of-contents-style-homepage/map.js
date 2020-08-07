d0fc1afb6243e4383eeec388d1c31ff4f69ea978
daniel coats <daniel.e.coats@gmail.com>
2020-08-07 15:02:00 -0700

    Switched to carto tile set.

--- table-of-contents-style-homepage/map.js
+++ table-of-contents-style-homepage/map.js
@@ -1,7 +1,9 @@
 function mapInit() {
-	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	//const airtable = require('airtable');
+	//airtable.configure({ endpointUrl: "https://api.airtable.com", apiKey: process.env.api_Key })
+	let mymap = L.map('mapid').setView([51.505, -0.09], 13);
 	var attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-	var tiles = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { attribution });
+	var tiles = L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png", { attribution });
 	tiles.addTo(mymap);
 }
 
