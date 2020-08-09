bfe2b261320983f84df068bc01c1635a8b8d8d18
daniel coats <daniel.e.coats@gmail.com>
2020-08-09 11:51:06 -0700

    Made some comments and changed some names.

--- table-of-contents-style-homepage/generateSelectOptions.js
+++ table-of-contents-style-homepage/generateSelectOptions.js
@@ -1,6 +1,7 @@
 // Mock data. This will be replaced with some sort of function to fetch data later.
 const cities = [{id: "rec1", name: "Eugene"}, {id: "rec2", name: "Portland"}, {id: "rec3", name: "Wisconsin Dells"}];
 
+// Generates multiple option elements
 function generateOptionElements(objArray) {
 	let elementString = "";
 	for (let i = 0; i < objArray.length; i++) {
@@ -9,12 +10,15 @@ function generateOptionElements(objArray) {
 	return elementString;
 }
 
+// Generates a single option element
 function generateOptionElement(obj) {
 	return `<option value=${obj.id}>${obj.name}</option>`;
 }
 
+// Writes an element string (objString) the element's innerHTML (elementId)
 function placeOptionElements(elementId, objString) {
 	document.getElementById(elementId).innerHTML = objString;
 }
 
+// Populate the city select
 placeOptionElements("citySelect", generateOptionElements(cities));
