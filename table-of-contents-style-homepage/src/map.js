70d07351fac8595b6f19f9c99bd6b0dba7250f1f
daniel coats <daniel.e.coats@gmail.com>
2020-08-09 14:08:03 -0700

    some code clean-up

--- table-of-contents-style-homepage/src/map.js
+++ table-of-contents-style-homepage/src/map.js
@@ -1,6 +1,4 @@
 function mapInit() {
-	//const airtable = require('airtable');
-	//airtable.configure({ endpointUrl: "https://api.airtable.com", apiKey: process.env.api_Key })
 	let mymap = L.map('mapid').setView([51.505, -0.09], 13);
 	var attribution = '&copy; <a href="https://carto.com/">Carto</a>';
 	var tiles = L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png", { attribution });
