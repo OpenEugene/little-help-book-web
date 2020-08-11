373db0586086bbcbd36e96ef44e12f4dea3433da
Arthur Smid <smidarthur@gmail.com>
2020-08-11 11:11:05 -0700

    begin work on the list-map toggle button for the category page

--- table-of-contents-style-homepage/category.js
+++ table-of-contents-style-homepage/category.js
@@ -1,3 +1,5 @@
+// show and hide sidebar with the arrow
+
 $(document).ready(function(){
     $(".show-arrow").hide();
     $(".hide-arrow").click(function () {
@@ -20,3 +22,13 @@ function moveRightColumnLeft(){
 function moveRightColumnRight(){
     $(".right-column").css("margin-left", "3rem")
 }
+
+// list or map view options button
+
+$(function(){
+    $('[data-target]').on('click', function(){
+      var target = $(this).data('target');
+      $(target).siblings().hide().end().show();
+    });
+  });
+  
