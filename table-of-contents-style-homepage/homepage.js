71a05068173ca83865b451dc9c6518696c9637af
daniel coats <daniel.e.coats@gmail.com>
2020-08-20 15:19:45 -0700

    Added the emergency page.

--- table-of-contents-style-homepage/homepage.js
+++ table-of-contents-style-homepage/homepage.js
@@ -1,4 +1,3 @@
-
 //  The popup description appears when someone clicks the blue crisis text 
 
 function crisisPopup() {
@@ -56,7 +55,7 @@ function getMobilePageOne() {
           </main>
           <footer>Human service resource guide powered by 
             <h2 class="bold-inline h123-reset">White Bird Clinic</h2>
-          </footer>`
+          </footer>`;
 }
 
 function getMobilePageTwo() {
@@ -79,11 +78,11 @@ function getMobilePageTwo() {
               </span>
             </h3>
             <div class="outline-buttons">
-              <button class="emergency-lines-button">Emergency Crisis Lines</button>
+              <button class="emergency-lines-button" onclick="emergencyPage()">Emergency Crisis Lines</button>
               <button class="find-my-city-button">Find my City</button>
               <button class="find-a-service-button" onclick="mostSought()">Find a Service</button>
             </div>
-          </main>` 
+          </main>`;
 }
 
 function getMobilePageMostSought() {
@@ -107,17 +106,71 @@ function getMobilePageMostSought() {
                 <h2 class="mobile-category-name more-button h123-reset">More<span class="more-arrow"> ></h2>
               </div>
             </div>
-          </main>`
+          </main>`;
 }
 
+function getMobilePageEmergency() {
+  return `<header class="logo-title">
+              <img src="white-bird-on-black.png" alt="White Bird Clinic" width="42" height="42">
+              <h1 class="little-help-book">Little Help Book</h1>
+              <div class="black-top"></div>
+          </header>
+          <main class="content-container">
+            <p class="how-to-call">Touch blue text to call.</p>
+            <div class="emergency-container">
+              <div class="emergency-line">
+                <h2 class="emergency-number h123-reset"><a href="tel:911">911</a></h2>
+              </div>
+              <div class="emergency-line">
+                <h2 class="emergency-number h123-reset"><a href="tel:541-682-5111">CAHOOTS – Eugene only – 541-682-5111</a></h2>
+                <div class="divider-line">––––––––––––––––––––––––––––</div>
+                <h2 class="emergency-number h123-reset"><a href="tel:541-726-3714">CAHOOTS – Springfield only – 541-726-3714</a></h2>
+                <p class="emergency-description">Provides on-site care for mental health or substance abuse emergencies, conflict resolution/mediation, transport to other agencies, deals with housing crises and first aid. Dispatched by police non-emergency line. Hours: all day, every day.</p>
+              </div>
+              <div class="emergency-line">
+                <h2 class="emergency-number h123-reset"><a href="tel:1-800-422-4453">National Child Abuse Hotline – 1-800-422-4453</a></h2>
+                <p class="emergency-description">Available all day, every day.</p>
+              </div>
+              <div class="emergency-line">
+                <h2 class="emergency-number h123-reset"><a href="tel:541-485-6513">Womenspace – 541-485-6513</a></h2>
+                <p class="emergency-description">For all victims and survivors of domestic violence, intimate partner violence, sexual assault and stalking in Lane County. Available all day, every day.</p>
+              </div>
+              <div class="emergency-line">
+                <h2 class="emergency-number h123-reset"><a href="tel:1-888-989-9990">Looking Glass – 1-888-989-9990</a></h2>
+                <p class="emergency-description">Crisis prevention helpline for families with children or homeless youth ages 17 and under. Available all day, every day.</p>
+              </div>
+              <div class="emergency-line">
+                <h2 class="emergency-number h123-reset"><a href="tel:1-800-273-8255">Thoughts of Suicide – 1-800-273-8255</a></h2>
+                <p class="emergency-description">National Suicide Prevention Lifeline. Available all day, every day.</p>
+              </div>
+              <div class="emergency-line">
+                <h2 class="emergency-number h123-reset"><a href="tel:1-877-565-8860">Trans Lifeline – 1-877-565-8860</a></h2>
+                <p class="emergency-description">Provides peer counseling for transgender individuals in crisis. Available every day, beginning 7:00am and through the day to past midnight, closing at 1:00am.</p>
+              </div>
+              <div class="emergency-line">
+                <h2 class="emergency-number h123-reset"><a href="tel:1-866-488-7386">Trevor Lifeline – 1-866-488-7386</a></h2>
+                <p class="emergency-description">A crisis intervention and suicide prevention phone service for LGBTQIA. Or, text “START” to 678678 (standard text messaging rates apply). Available all day, every day.</p>
+              </div>
+              <div class="emergency-line">
+                <h2 class="emergency-number h123-reset"><a href="tel:541-687-4000">White Bird 24/7 Crisis Line – 541-687-4000</a></h2>
+                <div class="divider-line">––––––––––––––––––––––––––––</div>
+                <h2 class="emergency-number h123-reset"><a href="tel:1-800-422-7558">White Bird 24/7 Crisis Line – 1-800-422-7558</a></h2>
+                <p class="emergency-description">Crisis intervention, mental health referral and transportation assistance for people with mental health needs in Eugene or Springfield without homes. Available all day, every day.</p>
+              </div>
+            </div>
+          </main>`;
+}
 
 function findHelp() {
   document.getElementById("mobile-container").innerHTML = getMobilePageTwo();
 }
 
 function mostSought() {
-  console.log("mostSought");
   document.getElementById("mobile-container").innerHTML = getMobilePageMostSought();
+}
+
+function emergencyPage() {
+  document.getElementById("mobile-container").innerHTML = getMobilePageEmergency();
 }
 
 function mobileCrisisPopup() {
