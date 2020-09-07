33ebf1251b1e0dfe810594212f672bf66c6ed4cf
Yichen Zhang <zhangyichen9@gmail.com>
2020-09-07 12:48:22 -0400

    Added basics to get location and categories from airtable, plus set up Vue store to keep retrieved data

--- lhb-vue/src/services/AirtableService.js
+++ lhb-vue/src/services/AirtableService.js
@@ -1,8 +1,18 @@
 import axios from 'axios'
 
+// Read-only key
 const https = axios.create({
-    baseURL: "https://api.airtable.com/v0/appj3UWymNh6FgtGR",
+    baseURL: 'https://api.airtable.com/v0/appj3UWymNh6FgtGR/api/',
     headers: {
-        'Authorization': ''
+        'Authorization': 'Bearer keyMmAL4mVBSORkGc'
     }
-})
+});
+
+export default {
+    getPlaces() {
+        return https.get('help services');
+    },
+    getCategories() {
+        return https.get('categories');
+    }
+}
--- lhb-vue/src/store/index.js
+++ lhb-vue/src/store/index.js
@@ -5,11 +5,34 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    places: [],
+    categories: [],
+    activePlace: {
+      id: '',
+      name: '',
+      nameSpanish: '',
+      phone: '',
+      address: '',
+      categories: [],
+      subcategories: [],
+      latitude: 0,
+      longitude: 0,
+      url: '',
+      email: '',
+      hours: '',
+      description: ''
+    }
   },
   mutations: {
+    SET_ACTIVE_PLACE(state, data) {
+      state.activePlace = data;
+    },
+    SET_PLACES(state, data) {
+      state.places = data;
+    },
+    SET_CATEGORIES(state, data) {
+      state.categories = data;
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  strict: true
 })
