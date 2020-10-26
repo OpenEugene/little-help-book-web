ec4085ed8cac941faaf6c6648c2653dca03670c0
Dave Wolfe <dave@red58.com>
2020-10-26 11:33:47 -0700

    Add search form with show/hide toggles

---
+++ table-of-contents-style-homepage/src/search.js
@@ -0,0 +1,26 @@
+(function(){
+
+  const searchWrapper = document.getElementById('searchWrapper');
+  const searchInput   = document.getElementById('searchInput');
+  let searchToggles   = document.querySelectorAll(".search-toggle");
+
+  searchToggles.forEach(toggle => {
+    toggle.addEventListener('click', event => {
+      if (searchWrapper.classList.contains('hidden')) {
+        searchToggles.forEach(t => {
+          t.classList.add('searchIsActive');
+          t.setAttribute('src', 'images/close-24px.svg');
+        });
+        searchWrapper.classList.remove('hidden');
+        // NOTE: We should probably focus the search input field here
+      } else {
+        searchToggles.forEach(t => {
+          t.classList.remove('searchIsActive');
+          t.setAttribute('src', 'images/search-24px.svg');
+        });
+        searchWrapper.classList.add('hidden');
+      }
+    });
+  });
+
+})();
