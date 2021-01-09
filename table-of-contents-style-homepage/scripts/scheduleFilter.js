1b63c868c0b51c90f566c60f75b6bfe32d4f3a75
Rees Dart <thedominicparker@gmail.com>
2021-01-09 13:39:15 -0800

    schedule filter function working, but comprehensive testing still needed.

--- table-of-contents-style-homepage/scripts/scheduleFilter.js
+++ table-of-contents-style-homepage/scripts/scheduleFilter.js
@@ -17,7 +17,10 @@ let servicesObjects = [
 ]
 
 // Mock data date/times
-let dateTimeString = 'September 29, 2020 10:30:00';  // format example = 'December 17, 1995 03:24:00'
+let dateTimeObjects = [
+  { dateTimeString: 'September 29, 2020 10:30:00' },  // format example = 'December 17, 1995 03:24:00'
+  { dateTimeString: 'October 29, 2020 10:30:00' },  
+]
 
 const WEEKDAYS_OBJ = {
   1: "SU",
@@ -44,106 +47,114 @@ function isValidFromTo(schedule_item, dateTime) {
   let validFromArray = schedule_item["valid_from"].split("/").map(element => Number(element));
   let validToArray = schedule_item["valid_to"].split("/").map(element => Number(element));
 
-  // if validFromArray and validToArray don't include the year values 
-  if (!validFromArray[2] && !validToArray[2]) {
-    // and if validFromArray month is less than or equal to validToArray month
-    if (validFromArray[0] <= validToArray[0]) {
-      // push current year into validToArray
-      validToArray.push(dateTime.getFullYear());
-    } else { // if validFromArray month is greater that validToArray month
-      // push current year plus 1 into validToArray
-      validToArray.push(dateTime.getFullYear() + 1);
-    }
-  }
-  // push current year into validFromArray
-  validFromArray.push(dateTime.getFullYear());
-
+  // if validFromArray has value and validToArray does not
   if ((validFromArray !== []) && (validToArray === [])) {
+    // if validFromArray doesn't have year value, give current year value
+    if (!validFromArray[2]) validFromArray.push(dateTime.getFullYear());
+    // then return whether this is before or equal to current dateTime
     return (validGetTime(validFromArray) <= dateTime.getTime());
-  } else if ((validFromArray === []) && (validToArray !== [])) {
+  } else if ((validFromArray === []) && (validToArray !== [])) { // if validTo has value, but validFrom is empty
+    // if validToArray doesn't have year value, give current year value
+    if (!validToArray[2]) validToArray.push(dateTime.getFullYear());
     return (validGetTime(validToArray) >= dateTime.getTime());
   } else { // if both valid_from and valid_to have dates in schedule table
+    // if validFromArray and validToArray don't include the year values 
+    if (!validFromArray[2] && !validToArray[2]) {  // this assumes that we don't situation where one includes the year and the other doesn't.
+      // and if validFromArray month is less than or equal to validToArray month
+      if (validFromArray[0] <= validToArray[0]) {
+        // push current year into validToArray
+        validToArray.push(dateTime.getFullYear());
+      } else { // if validFromArray month is greater that validToArray month
+        // push current year plus 1 into validToArray
+        validToArray.push(dateTime.getFullYear() + 1);
+      }
+    }
+    // push current year into validFromArray
+    validFromArray.push(dateTime.getFullYear());
     let validFromGetTime = validGetTime(validFromArray);
     let validToGetTime = validGetTime(validToArray);
     return (dateTime.getTime() >= validFromGetTime && dateTime.getTime() <= validToGetTime)
   }
+}
 
-  // helper function splits byday string into array, then filters and verifies day elements
-  function isValidDay(schedule_item, monthDay, weekday, year, month) {
-    let bydayArray = schedule_item["byday"].split(', ');
-    let bydayMatchCurrentDay = bydayArray.filter(itemDay => itemDay.includes(WEEKDAYS_OBJ[weekday]));
-    if (bydayMatchCurrentDay.length > 0) {
-      for (day of bydayMatchCurrentDay) {
-        if (day.search(/[0-5]/) === -1) return true;
-        if (Number.parseInt(day, 10) === Math.ceil(monthDay / 7)) return true;
-        if (Math.sign(Number.parseInt(day, 10) === -1)) {
-          let rawDateLastDayOfMonth = new Date(year, month, 0); // e.g. 2020-12-31T08:00:00.000Z 
-          let lastDayNumOfMonth = rawDateLastDayOfMonth.getDate();
-          if ((monthDay >= lastDayNumOfMonth + (Number.parseInt(day, 10) * 7)) &&
-            (lastDayNumOfMonth + (Number.parseInt(day, 10) + 1) * 7) > monthDay) return true;
-        }
+// helper function splits byday string into array, then filters and verifies day elements
+function isValidDay(schedule_item, monthDay, weekday, year, month) {
+  let bydayArray = schedule_item["byday"].split(', ');
+  let bydayMatchCurrentDay = bydayArray.filter(itemDay => itemDay.includes(WEEKDAYS_OBJ[weekday]));
+  if (bydayMatchCurrentDay.length > 0) {
+    for (day of bydayMatchCurrentDay) {
+      if (day.search(/[0-5]/) === -1) return true;
+      if (Number.parseInt(day, 10) === Math.ceil(monthDay / 7)) return true;
+      if (Math.sign(Number.parseInt(day, 10) === -1)) {
+        let rawDateLastDayOfMonth = new Date(year, month, 0); // e.g. 2020-12-31T08:00:00.000Z 
+        let lastDayNumOfMonth = rawDateLastDayOfMonth.getDate();
+        if ((monthDay >= lastDayNumOfMonth + (Number.parseInt(day, 10) * 7)) &&
+          (lastDayNumOfMonth + (Number.parseInt(day, 10) + 1) * 7) > monthDay) return true;
       }
     }
-
-    return false;
   }
 
-  function isValidMonthDay(schedule_item, monthDay) {
-    let byMonthDayArray = schedule_item["bymonthday"].split(', ').map(stringNum => Number(stringNum));
-    for (byMonthDay of byMonthDayArray) {
-      if (byMonthDay === monthDay) return true;
-    }
-    return false;
+  return false;
+}
+
+function isValidMonthDay(schedule_item, monthDay) {
+  let byMonthDayArray = schedule_item["bymonthday"].split(', ').map(stringNum => Number(stringNum));
+  for (byMonthDay of byMonthDayArray) {
+    if (byMonthDay === monthDay) return true;
   }
+  return false;
+}
 
-  const MINS_PER_HR = 60;
+const MINS_PER_HR = 60;
 
-  // helper function returns equivalent exact minute in 1440 minute day
-  function getExactMinute(itemTime) {
-    let hour = Number(itemTime.substring(0, 2));
-    let minutes = Number(itemTime.substring(3));
-    return (hour * MINS_PER_HR) + minutes;
-  }
-  // helper function 
-  function isValidTime(schedule_item, hour, minute) {
-    let openingMinute = getExactMinute(schedule_item["opens_at"]);
-    let closingMinute = getExactMinute(schedule_item["closes_at"]);
-    let minuteOfDay = (hour * MINS_PER_HR) + minute;
-    return (minuteOfDay >= openingMinute && minuteOfDay <= closingMinute);
-  }
+// helper function returns equivalent exact minute in 1440 minute day
+function getExactMinute(itemTime) {
+  let hour = Number(itemTime.substring(0, 2));
+  let minutes = Number(itemTime.substring(3));
+  return (hour * MINS_PER_HR) + minutes;
+}
+// helper function 
+function isValidTime(schedule_item, hour, minute) {
+  let openingMinute = getExactMinute(schedule_item["opens_at"]);
+  let closingMinute = getExactMinute(schedule_item["closes_at"]);
+  let minuteOfDay = (hour * MINS_PER_HR) + minute;
+  return (minuteOfDay >= openingMinute && minuteOfDay <= closingMinute);
+}
 
-  // main function - checks whether item in schedule table is open (returns true), or closed (returns false)
-  function isOpen(schedule_item, dateTimeString) {
-    let dateTime = new Date(dateTimeString)
-    let year = dateTime.getFullYear();
-    let month = dateTime.getMonth() + 1; //   (Date.getMonth() starts at 0 so add 1)
-    let weekday = dateTime.getDay() + 1; //   (Date.getDay() starts at 0 so add 1)
-    let monthDay = dateTime.getDate(); //   (Date.getDate() starts at 1)
-    let hour = dateTime.getHours(); //   (Date.getHours() starts at 0)
-    let minute = dateTime.getMinutes(); //   (Date.getMinutes() starts at 0)
+// main function - checks whether item in schedule table is open (returns true), or closed (returns false)
+function isOpen(schedule_item, dateTimeString) {
+  let dateTime = new Date(dateTimeString)
+  let year = dateTime.getFullYear();
+  let month = dateTime.getMonth() + 1; //   (Date.getMonth() starts at 0 so add 1)
+  let weekday = dateTime.getDay() + 1; //   (Date.getDay() starts at 0 so add 1)
+  let monthDay = dateTime.getDate(); //   (Date.getDate() starts at 1)
+  let hour = dateTime.getHours(); //   (Date.getHours() starts at 0)
+  let minute = dateTime.getMinutes(); //   (Date.getMinutes() starts at 0)
 
-    // if there is valid_from and valid_to fields, verify date falls between these?
-    if (!isValidFromTo(schedule_item, dateTime)) return false;
-    // if there is a value in the byday column, very it/one of these correlates to the current day?
-    if (schedule_item["byday"] !== "") {
-      if (isValidDay(schedule_item, monthDay, weekday, year, month)) {
-        return isValidTime(schedule_item, hour, minute);
-      }
-    } else if (isValidMonthDay(schedule_item, monthDay)) {
+  // if there is valid_from and valid_to fields, verify date falls between these?
+  if (!isValidFromTo(schedule_item, dateTime)) return false;
+  // if there is a value in the byday column, very it/one of these correlates to the current day?
+  if (schedule_item["byday"] !== "") {
+    if (isValidDay(schedule_item, monthDay, weekday, year, month)) {
       return isValidTime(schedule_item, hour, minute);
-    } else return false;
-  }
+    }
+  } else if (isValidMonthDay(schedule_item, monthDay)) {
+    return isValidTime(schedule_item, hour, minute);
+  } else return false;
+}
 
-  // function to run through each test case in servicesObjects
-  function test(servicesObjects, dateTimeString, WEEKDAYS_OBJ) {
-    let currentDate = new Date(dateTimeString);
-    console.log(`CURRENT DATETIME INPUT: ${WEEKDAYS_OBJ[(currentDate.getDay() + 1)]}, ${dateTimeString}`);
+// function to run through each test case in servicesObjects
+function test(servicesObjects, dateTimeObjects, WEEKDAYS_OBJ) {
+  dateTimeObjects.forEach(dateTimeObject => {
+    let currentDateTime = new Date(dateTimeObject.dateTimeString);
+    console.log(`CURRENT DATETIME INPUT: ${WEEKDAYS_OBJ[(currentDateTime.getDay() + 1)]}, ${dateTimeObject.dateTimeString}`);
     servicesObjects.forEach(item => {
-      if (isOpen(item, dateTimeString)) {
+      if (isOpen(item, dateTimeObject.dateTimeString)) {
         console.log(`${item["id"]} is OPEN ==>>> ${Object.values(item)}`);
       } else console.log(`${item["id"]} is CLOSED ==>>> ${Object.values(item)}`);
-    });
-  }
+    })
+  })
+}
 
-  // run test
-  test(servicesObjects, dateTimeString, WEEKDAYS_OBJ);
+// run test
+test(servicesObjects, dateTimeObjects, WEEKDAYS_OBJ);
